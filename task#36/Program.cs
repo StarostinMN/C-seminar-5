85a69b64b53880236a96ed03fcbf90bb8419d8c3
Mikhail Starostin <starostin.mihail@mail.ru>
2022-12-08 13:19:09 +0300

    add task#36

---
+++ task#36/Program.cs
@@ -0,0 +1,38 @@
+﻿/* Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19, [-4, -6, 89, 6] -> 0 */
+Console.Clear();
+
+Console.WriteLine("Введите размерность массива");
+int size = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите минимальное допустимое значение в массиве");
+int min = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите максимальное допустимое значение в массиве");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] Array = FillArray(size, min, max);
+
+Console.WriteLine($"Получившийся массив: [{string.Join(",", Array)}]");
+Console.WriteLine($"Сумма элементов массива, стоящих на нечетных позициях, равна {ComputeSumOfOddPositionElements(Array)}");
+
+int[] FillArray(int sizeArray, int minValue, int maxValue)
+{
+    Random random = new Random();
+    int[] resultArray = new int[sizeArray];
+    for (int i = 0; i < resultArray.Length; i++)
+    {
+        resultArray[i] = random.Next(minValue, maxValue + 1);
+    }
+    return resultArray;
+}
+
+int ComputeSumOfOddPositionElements(int[] array)
+{
+    int sum = 0;
+    for (int i = 1; i < array.Length; i = i + 2)
+    {
+        sum = sum + array[i];
+    }
+    return sum;
+}
